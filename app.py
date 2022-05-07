c6a800f6abf8058ee8bf7e087442d6a7144fec4f
Wojtyua <wojciechderlikiewicz@gmail.com>
2022-05-07 14:15:15 +0200

    kutas psa
    kutas psa

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for, redirect, request
+from re import template
+import bcrypt
+from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
@@ -12,21 +14,29 @@ bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'Ww9VH.k:XfJr#?%B' #ranodm generated string
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 #creating db table
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(30), nullable=False, unique=True)
     name = db.Column(db.String(20), nullable=False)
-    last_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(80), nullable=False)
 
 #register form
 class RegisterForm(FlaskForm):
-
     email = StringField(validators=[InputRequired(), Length(min = 4, max=30)], render_kw={"placeholder": "Email"})
-    name = StringField(validators=[InputRequired(), Length(min = 4, max=20)], render_kw={"placeholder": "Name"})
-    last_name = StringField(validators=[InputRequired(), Length(min = 4, max=30)], render_kw={"placeholder": "Last name"})
-    password = PasswordField(validators=[InputRequired(), Length(min = 4, max=80)], render_kw={"placeholder": "Password"})
+    name = StringField(validators=[InputRequired(), Length(min = 4, max=20)], render_kw={"placeholder": "Imie"})
+    last_name = StringField(validators=[InputRequired(), Length(min = 4, max=20)], render_kw={"placeholder": "Nazwisko"})
+    password = PasswordField(validators=[InputRequired(), Length(min = 4, max=20)], render_kw={"placeholder": "Hasło"})
 
     submit = SubmitField("Register")
 
@@ -45,33 +55,39 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Login")
 
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-       # user = User.query.filter_by(email=form.email.data).first()
-       # if user:
-           # if bcrypt.check_password_hash(user.password, form.password.data):
-               # login_user(user)
-               # return redirect(url_for('dashboard'))
-        return "<h1>" + form.username.data
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+
     return render_template('login.html', form=form)
 
-@app.route('/register', methods=['GET', 'POST'])
+
+@app.route('/dashboard', methods=['GET','POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/register', methods=['GET','POST'])
 def register():
     form = RegisterForm()
-    
+
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
         new_user = User(name=form.name.data, email=form.email.data, last_name=form.last_name.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
-        
 
     return render_template('register.html', form=form)
 
